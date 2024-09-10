3643f5178aabd7324b1f727e736ed3d22d083b41
MracusRin <aleksandrovi44@gmail.com>
2024-09-10 19:34:39 +0500

    Create Board.java for print game board

--- src/main/java/org/mracus/corp/Board.java
+++ src/main/java/org/mracus/corp/Board.java
@@ -1,16 +1,28 @@
 package org.mracus.corp;
 
 public class Board {
-    public void drawBoard() {
-        System.out.println("""
+    public void state(int errorCount, String word, String errorLetter, String currentLetter) {
+        drawBoard(errorCount);
+        System.out.println("Слово: " + word);
+        System.out.printf("Ошибки (%s): %s\n", errorCount, errorLetter);
+        System.out.println("Буква: " + currentLetter);
+    }
+
+    private void drawBoard(int count) {
+        String[] pattern = {"(_)", "|", "/", "\\", "/", "\\"};
+        String[] placeHolder = {" ", " ", " ", " ", " ", " "};
+        for (int i = 0; i < count; i++) {
+            placeHolder[i] = pattern[i];
+        }
+        System.out.printf("""
                      _________
                      |       |
-                     |
-                     |
-                     |
+                     |      %s
+                     |      %s%s%s
+                     |      %s %s
                      |
                    __|__
                 __/_____\\________
-                """);
+                """, placeHolder[0], placeHolder[2], placeHolder[1], placeHolder[3], placeHolder[4], placeHolder[5]);
     }
 }
--- src/main/java/org/mracus/corp/Main.java
+++ src/main/java/org/mracus/corp/Main.java
@@ -2,8 +2,7 @@ package org.mracus.corp;
 
 public class Main {
     public static void main(String[] args) {
-        Board draw = new Board();
-        draw.drawBoard();
-        System.out.println("BaldaGame!");
+        Board board = new Board();
+        board.state(6, "hello", "A, D", "D");
     }
 }
